a90eb781f69e747dc060ac611e85517ee62a49cd
Seyifunmi Owoeye <seyifunmiowoeye@gmail.com>
2026-05-11 03:07:03 -0400

    update plotting colors to improve distinguishability

--- src/metrics/Constants.jl
+++ src/metrics/Constants.jl
@@ -4,6 +4,6 @@ const MIN_REQUIRED_COLS = 3
 
 const DEFAULT_COLORS = [
     "#377eb8", "#ff7f00", "#4daf4a", "#e41a1c", "#984ea3", "#a65628",
-    "#f781bf", "#00ced1", "#000000", "#5A9D5A", "#D96D3B", "#FFAD12",
+    "#f781bf", "#00ced1", "#000000", "#56B4E9", "#D96D3B", "#FFAD12",
     "#66628D", "#91569A", "#B6742A", "#DD87B4", "#D26D7A", "#DAA520", "#dede00"
 ]
